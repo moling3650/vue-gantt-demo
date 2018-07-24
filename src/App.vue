f210b76c80a6efc6a092358366ecb3455aeeb8fc
moling <365024424@qq.com>
2018-07-24 19:45:35 +0800

    更新样式

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,58 @@ export default {
 </script>
 
 <style>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+.container {
+  width: 100%;
+  height: 100%;
+}
+.left-container {
+  overflow: hidden;
+  position: relative;
+  height: 100%;
+}
+.right-container {
+  border-right: 1px solid #cecece;
+  float: right;
+  height: 100%;
+  width: 340px;
+  box-shadow: 0 0 5px 2px #aaa;
+  position: relative;
+  z-index:2;
+}
+.gantt-messages {
+  list-style-type: none;
+  height: 50%;
+  margin: 0;
+  overflow-x: hidden;
+  overflow-y: auto;
+  padding-left: 5px;
+}
+.gantt-messages > .gantt-message {
+  background-color: #f4f4f4;
+  box-shadow:inset 5px 0 #d69000;
+  font-family: Geneva, Arial, Helvetica, sans-serif;
+  font-size: 14px;
+  margin: 5px 0;
+  padding: 8px 0 8px 10px;
+}
+.gantt-selected-info {
+border-bottom: 1px solid #cecece;
+  box-sizing: border-box;
+  font-family: Geneva, Arial, Helvetica, sans-serif;
+  height: 50%;
+  line-height: 28px;
+  padding: 10px;
+}
+.gantt-selected-info h2 {
+  border-bottom: 1px solid #cecece;
+}
+
+.select-task-prompt h2{
+  color: #d9d9d9;
+}
 </style>
